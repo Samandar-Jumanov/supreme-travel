25621ba34f4af53987c9a59986bf4e7362b3a17f
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-02-02 06:47:47 -0500

    added flight schema

--- models/flights.js
+++ models/flights.js
@@ -1,5 +1,49 @@
 import { model , models , Schema } from 'mongoose';
 
 const FlightSchema = new Schema({
-      
+      user : {
+        type : Schema.Types.ObjectId,
+         ref :'User'
+      },
+              
+      from :{
+         type : String , 
+         required : true
+      },
+      to : {
+          type : String,
+          required : true
+      },
+      ticket : {
+          type : String,
+          required : true
+      },
+      price : {
+         type : String,
+         required : true
+      },
+      date :  {
+         type : String ,
+         required : true
+      },
+      place : {
+        type : String,
+        required : true
+      },
+      time : {
+        type : String,
+        required : true
+      },
+      type : {
+        type : String,
+        required : true
+      },
+      meals : {
+        type : String,
+        required : true
+      }
 })
+
+
+const Flight =  models.Flight || model(FlightSchema , "Flights")
+export default Flight;
