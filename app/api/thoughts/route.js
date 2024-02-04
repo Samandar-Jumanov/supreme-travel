590116be223f1ba107f601e3e2f88ef74198f853
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-02-04 00:37:42 -0500

    added get all thoughts

---
+++ app/api/thoughts/route.js
@@ -0,0 +1,15 @@
+import models from "@/models/models";
+import connectDb from "@/utils/connectMongo";
+
+export const GET = async ( request ) =>{
+    try {
+          await connectDb();
+          const allThoughts = await models.Flights.find({});
+          return new Response(JSON.stringify(allThoughts , { status : 200}))
+    } catch (error) {
+          console.log(error.message);
+          return new Response(JSON.stringify(error, { status : 500}))
+    }
+};
+
+
