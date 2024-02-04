3a20b1c904ce0a089e971af17f47e67de898fc36
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-02-04 12:03:16 -0500

    added a bit styling

--- app/thoughtInfo/page.jsx
+++ app/thoughtInfo/page.jsx
@@ -27,21 +27,25 @@ const ThoughtInfo = () => {
   }, [thoughtId]);
 
   return (
-    <div className="container mx-auto mt-8 p-4">
+    <div className="container mx-auto mt-8 p-4 max-w-md">
       {thoughtInfo ? (
         <div className="text-center">
-          <h1 className="text-3xl font-bold mb-4">{thoughtInfo.thoughtText}</h1>
-          <div className="mb-4">
-            <h1 className="text-lg font-semibold">{thoughtInfo.username}</h1>
-            <h1 className="text-sm text-gray-500">{thoughtInfo.userEmail}</h1>
-            {/* <Image
-              src={thoughtInfo?.userImage}
-              alt='user_image'
-              width={50}
-              height={60}
-              className="rounded-full"
-            /> */}
+          <div className="mb-4 flex items-center justify-center">
+            <div className="mr-4">
+              <Image
+                src="https://lh3.googleusercontent.com/a/ACg8ocLIAyDpUQ-yigGjY1yR3S_YvNx1bmHtj5sIS75nUAPX=s96-c"
+                alt='user_image'
+                width={50}
+                height={60}
+                className="rounded-full"
+              />
+            </div>
+            <div>
+              <h1 className="text-lg font-semibold">{thoughtInfo.username}</h1>
+              <h1 className="text-sm text-gray-500">{thoughtInfo.userEmail}</h1>
+            </div>
           </div>
+          <h1 className="text-3xl font-bold mb-4">{thoughtInfo.thoughtText}</h1>
           <p className="mb-4">Did you find it helpful?</p>
           <div className="flex justify-center space-x-4">
             <button className="bg-green-500 text-white px-4 py-2 rounded">Yes</button>
